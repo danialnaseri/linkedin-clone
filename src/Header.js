584def6e33dd33ce36531098c65a0f99b42db4dc
Danial Naseri <daniel.naseri24@gmail.com>
2021-04-19 13:32:30 +0200

    Updating changes

--- src/Header.js
+++ src/Header.js
@@ -22,24 +22,26 @@ const Header = () => {
 
   return (
     <div className="header">
-      <div className="header__left">
-        <img
-          src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg"
-          alt=""
-        />
-        <div className="header__search">
-          <SearchIcon />
-          <input placeholder="Search" type="text" />
+      <div className="header_container">
+        <div className="header__left">
+          <img
+            src="https://www.flaticon.com/svg/static/icons/svg/174/174857.svg"
+            alt=""
+          />
+          <div className="header__search">
+            <SearchIcon />
+            <input placeholder="Search" type="text" />
+          </div>
         </div>
-      </div>
 
-      <div className="header__right">
-        <HeaderOption Icon={HomeIcon} title="Home" />
-        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
-        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
-        <HeaderOption Icon={ChatIcon} title="Messaging" />
-        <HeaderOption Icon={NotificationsIcon} title="Notications" />
-        <HeaderOption avatar={true} title="me" onClick={logoutOfApp} />
+        <div className="header__right">
+          <HeaderOption Icon={HomeIcon} title="Home" />
+          <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+          <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+          <HeaderOption Icon={ChatIcon} title="Messaging" />
+          <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+          <HeaderOption avatar={true} title="me" onClick={logoutOfApp} />
+        </div>
       </div>
     </div>
   );
